877a5ce433a61ed71239e0068ff54a71c77e1dc9
ksandwojtek <ksandwojtek@gmail.com>
2023-11-19 21:43:37 +0100

    Added caught fish counter

--- src/main/java/net/ksndq/fishinghelper/FishingHelper.java
+++ src/main/java/net/ksndq/fishinghelper/FishingHelper.java
@@ -1,23 +1,18 @@
 package net.ksndq.fishinghelper;
 
-import com.mojang.logging.LogUtils;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
+
 
 @Mod(FishingHelper.MOD_ID)
 public class FishingHelper {
     public static final String MOD_ID = "fishinghelper";
-    private static final Logger LOGGER = LogUtils.getLogger();
     public static boolean enabled;
     public static String prefix;
 
@@ -29,13 +24,8 @@ public class FishingHelper {
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event) {
-
-    }
-
     @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-
+    public void commonSetup(final FMLCommonSetupEvent event) {
     }
 
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
